efaf98b7915d7f4b42ec5b4ec26a6b37c3e98966
Ken Hawkins <khawkins98@gmail.com>
2020-03-07 14:01:25 +0100

    SW fixes

--- src/site/sw.js
+++ src/site/sw.js
@@ -1,54 +1,65 @@
-// via http://www.manifoldjs.com/generator
-//Install stage sets up the offline page in the cahche and opens a new cache
-self.addEventListener('install', function(event) {
-  event.waitUntil(preLoad());
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+
+      if (offlineFallbackPage === "ToDo-replace-this-name.html") {
+        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+      }
+      
+      return cache.add(offlineFallbackPage);
+    })
+  );
 });
 
-var preLoad = function() {
-  // console.log('[PWA Builder] Install Event processing');
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    // console.log('[PWA Builder] Cached index and offline page during Install');
-    // return cache.addAll(['/offline', '/']);
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("[PWA Builder] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(function (error) {        
+        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return error page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
   });
 }
 
-self.addEventListener('fetch', function(event) {
-  event.waitUntil(addToCache(event.request));
-  // console.log('The service worker is serving the asset.');
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request)}
-  ));
-});
-
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response)
-      } else {
-        reject()
-      }
-    }, reject)
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
   });
-};
-
-var addToCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
-    return fetch(request).then(function (response) {
-      // console.log('[PWA Builder] add page to offline'+response.url)
-      return cache.put(request, response);
-    });
-  });
-};
-
-var returnFromCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match('offline')
-     } else {
-       return matching
-     }
-    });
-  });
-};
+}
